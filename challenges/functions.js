868bf349b793ed50c63073ce4d8fb3e97120a9fb
Blake Florence <bflorencedev@gmail.com>
2019-11-15 10:19:30 -0800

    mvps met for sprint

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,7 +6,9 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+function consume (a, b, cb) {
+  return cb(a, b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,6 +16,17 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add (a, b) {
+  return a + b;
+}
+
+function multiply (a, b) {
+  return a * b;
+}
+
+function greeting (firstName, lastname) {
+  return `Hello ${firstName} ${lastname}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // console.log(consume(2, 2, add)); // 4
@@ -25,7 +38,7 @@
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
+// Explanation: With closure, inner functions have access to the variables of outer functions.
 
 
 const external = "I'm outside the function";
